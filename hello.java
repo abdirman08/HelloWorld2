7c4de70f982a6edbd66abdc554132305d65dac76
Abdifatah Abdirahman <abdifatahabdirahman@MacBookPro.lan>
2025-02-07 10:54:03 -0500

    Add modified java file with println greeting Bob

--- hello.java
+++ hello.java
@@ -4,7 +4,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		
+		System.out.println("Hi Alice!");
+		System.out.println("Hi Bob!");
 	}
 
 }
